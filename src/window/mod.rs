a1598be87ba9ced7fe54ff6e092d9b9436b1a27f
Alberto A. <reviraemusic@gmail.com>
2024-03-17 17:31:22 -0300

    form <=> list CRUD reactivity progress

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,16 +1,21 @@
 mod window;
 
+use adw::ActionRow;
 use gio::{ActionGroup, ActionMap};
 use glib::Object;
 use gtk::glib::clone;
-use gtk::glib::object::{Cast, ObjectExt};
 use gtk::glib::subclass::types::ObjectSubclassIsExt;
-use gtk::{gio, glib, pango, Label, ListBoxRow};
+use gtk::{
+    gio, glib, pango, FilterListModel, Label,
+    ListBoxRow, NoSelection,
+};
+use gtk::{prelude::*, CustomFilter};
 use gtk::{
     Accessible, Buildable, ConstraintTarget, Native,
     ShortcutManager,
 };
 
+use crate::food::FoodObject;
 use crate::food_collection::FoodCollection;
 
 glib::wrapper! {
@@ -38,25 +43,59 @@ impl ChefApp {
             .foodlist()
     }
 
+    pub fn foodcollections(&self) -> gio::ListStore {
+        let app = self.imp();
+        app.food_collections
+            .get()
+            .expect("`collections` should be set in `setup_collections`")
+            .clone()
+    }
+
+    fn new_food(&self) {
+        let app = self.imp();
+
+        let name = app.entry_name.text().to_string();
+        if name.is_empty() {
+            return;
+        }
+        app.entry_name.set_text("");
+
+        let brand =
+            app.entry_brand.text().to_string();
+        app.entry_brand.set_text("");
+
+        let new_food =
+            FoodObject::new(name, brand, 0, 0, 0);
+        self.foodlist().append(&new_food);
+    }
+
+    fn new_collection(&self) {
+        let foodlist =
+            gio::ListStore::new::<FoodObject>();
+
+        let name = "test".to_owned();
+        let foodlist =
+            FoodCollection::new(&name, foodlist);
+
+        self.foodcollections().append(&foodlist);
+        self.set_current_collection(foodlist);
+    }
+
     fn create_collection_row(
         &self,
         obj: &FoodCollection,
-    ) -> ListBoxRow {
-        let label_name = Label::builder()
-            .ellipsize(pango::EllipsizeMode::End)
-            .xalign(0.0)
-            .build();
-        obj.bind_property(
-            "name",
-            &label_name,
-            "label",
-        )
-        .sync_create()
-        .build();
+    ) -> adw::ActionRow {
+        // let label_name = Label::builder()
+        // .ellipsize(pango::EllipsizeMode::End)
+        // .xalign(0.0)
+        // .build();
+        let row = adw::ActionRow::new();
 
-        ListBoxRow::builder()
-            .child(&label_name)
-            .build()
+        obj.bind_property("name", &row, "title")
+            .sync_create()
+            .build();
+
+        row
     }
 
     fn setup_collections(&self) {
@@ -76,12 +115,82 @@ impl ChefApp {
         )
     }
 
+    fn create_food_row(
+        &self,
+        food_object: &FoodObject,
+    ) -> adw::ActionRow {
+        let row = ActionRow::builder().build();
+
+        food_object
+            .bind_property("name", &row, "title")
+            .sync_create()
+            .build();
+
+        row
+    }
+
+    fn set_current_collection(
+        &self,
+        collection: FoodCollection,
+    ) {
+        let app = self.imp();
+
+        let foodlist = collection.foodlist();
+        let filter_model = FilterListModel::new(
+            Some(foodlist.clone()),
+            self.filter(),
+        );
+        let selection_model =
+            NoSelection::new(Some(filter_model));
+
+        app.food_list.bind_model(Some(&selection_model),clone!(@weak self as window => @default-panic, move |obj| {
+            let food_object = obj.downcast_ref().expect("the object should be of type `FoodObject`");
+            let row = window.create_food_row(food_object);
+            row.upcast()
+        }));
+
+        app.current_fc.replace(Some(collection));
+    }
+
+    fn filter(&self) -> Option<CustomFilter> {
+        // let filter = CustomFilter::new()
+
+        // let _filter_x = CustomFilter::new(|obj| {
+        // let food_object = obj
+        // .downcast_ref::<FoodObject>()
+        // .expect("the object needs to be of type `FoodObject`");
+        // true//todo
+        // });
+
+        // match filter_state.as_str() {
+        //     "All" => None,
+        //     _ => unreachable!(),
+        // }
+        None
+    }
+
     pub fn setup(&self) {
         let app = self.imp();
 
+        // ActionGroupExt::activate_action(
+        //     self,
+        //     "chef.new-collection",
+        //     None,
+        // );
+        // spawn_blocking(self.new_collection);
+        // self.add_action()
+        // println!("{:?}", self.list_actions());
+        // .expect(
+        // "failed to create initial collection",
+        // );
+
+        self.new_collection();
         app.stack.set_visible_child_name("main");
 
-        // app.entry_name
-        // .add_prefix(&Label::new(Some(">>")));
+        app.button_submit.connect_clicked(
+            clone!(@weak self as window => move |_| {
+                window.new_food();
+            }),
+        );
     }
 }
--- src/window/window.rs
+++ src/window/window.rs
@@ -4,7 +4,7 @@ use adw::prelude::*;
 use adw::subclass::prelude::*;
 use gtk::gio::ListStore;
 use gtk::glib::subclass::InitializingObject;
-use gtk::{glib, ListBox, Stack};
+use gtk::{glib, Button, ListBox, Stack};
 
 use crate::food_collection::FoodCollection;
 
@@ -19,6 +19,8 @@ pub struct ChefApp {
     pub entry_name: TemplateChild<adw::EntryRow>,
     #[template_child]
     pub entry_brand: TemplateChild<adw::EntryRow>,
+    #[template_child]
+    pub button_submit: TemplateChild<Button>,
     //----
     pub food_collections: OnceCell<ListStore>,
     pub current_fc: RefCell<Option<FoodCollection>>,
@@ -42,6 +44,14 @@ impl ObjectSubclass for ChefApp {
     fn class_init(klass: &mut Self::Class) {
         klass.bind_template();
         klass.bind_template_callbacks();
+
+        klass.install_action_async(
+            "chef.new-collection",
+            None,
+            |window, _, _| async move {
+                window.new_collection();
+            },
+        );
     }
 
     fn instance_init(obj: &InitializingObject<Self>) {
@@ -53,8 +63,8 @@ impl ObjectImpl for ChefApp {
     fn constructed(&self) {
         self.parent_constructed();
         let obj = self.obj();
-        obj.setup();
         obj.setup_collections();
+        obj.setup();
         // obj.setup_callbacks();
         // obj.setup_users();
         // obj.setup_actions();
